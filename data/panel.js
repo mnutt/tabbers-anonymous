49a4338bc592346d4f42552c275e0f328894be0f
Michael Nutt <michael@nuttnet.net>
2014-10-09 23:01:49 -0400

    add tags

--- data/panel.js
+++ data/panel.js
@@ -18,8 +18,10 @@ addon.port.on('tab', function(tab) {
   var el = $("<div></div>").attr("id", tabId(tab)).addClass('tab');
   el.data('id', tab.id);
   var link = $("<a></a>").attr("href", tab.url).text(tab.title).appendTo(el);
-  var save = $("<button>Save</button>").appendTo(el);
+  var save = $("<button>Save</button>").addClass('save-button').appendTo(el);
   var remove = $("<button>Remove</button>").appendTo(el);
+  var tags = $("<div class='tags'>Tags:</div>").appendTo(el);
+  var tagInput = $("<input type='text'/>").appendTo(tags);
 
   link.on('click', function(e) {
     e.preventDefault();
@@ -66,15 +68,37 @@ function setRemove(el) {
 function nextItem(selected) {
   selected.removeClass('selected');
   selected.next().addClass('selected');
+  if(!selected.next().length) {
+    selected.siblings(':first').addClass('selected');
+  }
+}
+
+function previousItem(selected) {
+  selected.removeClass('selected');
+  selected.prev().addClass('selected');
+  if(!selected.prev().length) {
+    selected.siblings(':last').addClass('selected');
+  }
 }
 
 function done() {
-  var saveList = $(".tab.to-save").map(function() { return $(this).data('id'); }).get();
-  var removeList = $(".tab.to-remove").map(function() { return $(this).data('id'); }).get();
+  var bookmarkList = {};
+  var saveList = $(".tab.to-save").map(function() {
+    var bookmarks = $(this).find('input').val();
+    if(bookmarks.length > 0) {
+      bookmarkList[$(this).data('id')] = bookmarks.split(/,\s+/);
+    }
+    return $(this).data('id');
+  }).get();
+
+  var removeList = $(".tab.to-remove").map(function() {
+    return $(this).data('id');
+  }).get();
 
   addon.port.emit('done', {
     toRemove: removeList,
-    toSave: saveList
+    toSave: saveList,
+    bookmarks: bookmarkList
   });
 }
 
@@ -84,15 +108,34 @@ addon.port.on('removeTab', function(tab) {
 });
 
 $(document).on('keypress', function(e) {
-  if(e.which == 115 || e.which == 114) {
+  if($(".tags input:focus").length) {
+    if(e.which == 0) {
+      e.preventDefault();
+      var selected = $(".tab.selected");
+      nextItem(selected);
+    } else {
+      return;
+    }
+  }
+
+  if(e.which == 115 || e.which == 114 || e.which == 110 || e.which == 112 || e.which == 116) {
     var selected = $(".tab.selected");
     if(e.which == 115) { // s
       setSave(selected);
-    } else { // r
+      nextItem(selected);
+    } else if(e.which == 114) { // r
       setRemove(selected);
+      nextItem(selected);
+    } else if(e.which == 110) { // n
+      nextItem(selected);
+    } else if(e.which == 112) { // p
+      previousItem(selected);
+    } else if(e.which == 116) { // t
+      setSave(selected);
+      selected.find('input').focus();
     }
-    nextItem(selected);
   }
+
   if(e.which == 102) { // f
     done();
   }
